480f60ed386c7ff409d2b0f948ec95eaf5dfaf33
DanieLSBTH <josuemarque15@gmail.com>
2024-05-24 02:27:45 -0600

    m

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const session = require('express-session');
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://tu-farmacia-en-linea.onrender.com"
 };
 
 app.use(cors(corsOptions));
